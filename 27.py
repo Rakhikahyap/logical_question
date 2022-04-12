43617ad5e30d4ffdde22a901acdc0b076839d9c0
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-04-12 20:14:36 +0530

    Add files via upload

---
+++ 27.py
@@ -0,0 +1,26 @@
+# x=['R','U','J','A']
+# i=0
+# n=[]
+# while i<len(x):
+#     b=[(x[i])]
+#     n.append(b)
+#     i+=1
+# print(n)
+
+# age = 90
+# name = 'Some Name'
+# float=2.6
+# print(type(age))
+# print(type(name))
+# print(type(float)) 
+
+
+# a="5.0"
+# b="subhasree"
+# c=float(5.0)
+# e=int(5)
+# f=str(b)
+# print(e,f)
+
+# a=35
+# print('"35"')
